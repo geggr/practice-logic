04d8c302a735d9c3218294a6814c0cda9f69ce28
Geovani Granieri <geovani.granieri@outlook.com>
2023-12-04 10:26:51 -0300

    resolve first day as another way

--- src/main/java/br/com/university/days/FirstDay.java
+++ src/main/java/br/com/university/days/FirstDay.java
@@ -1,7 +1,12 @@
 package br.com.university.days;
 
+import static java.util.Map.entry;
+
 import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import br.com.university.utils.ProblemReader;
@@ -116,4 +121,67 @@ public class FirstDay implements PuzzleResolver {
             return String.valueOf(total);
         });
     }
+
+    private String reverse(String input){
+        return new StringBuilder(input).reverse().toString(); 
+    }
+
+    public String solveCompleteProblemInOtherWay(){
+
+        final var patterns = new TreeMap<String, String>(Map.ofEntries(
+            entry("1", "1"),
+            entry("2", "2"),
+            entry("3", "3"),
+            entry("4", "4"),
+            entry("5", "5"),
+            entry("6", "6"),
+            entry("7", "7"),
+            entry("8", "8"),
+            entry("9", "9"),
+            entry("one", "1"),
+            entry("two", "2"),
+            entry("three", "3"),
+            entry("four", "4"),
+            entry("five", "5"),
+            entry("six", "6"),
+            entry("seven", "7"),
+            entry("eight", "8"),
+            entry("nine", "9")
+        ));
+
+        final var defaultPattern = patterns.keySet().stream().collect(Collectors.joining("|"));
+        final var reversePattern = patterns.keySet().stream().map(pattern -> reverse(pattern)).collect(Collectors.joining("|"));
+
+        final var pattern = Pattern.compile(defaultPattern);
+        final var reversedPattern = Pattern.compile(reversePattern);
+
+        final var total = ProblemReader.read("problems/day-one", lines -> {
+            
+            return lines.map(line -> {
+                final var reversedLine = reverse(line);
+
+                final var matcher = pattern.matcher(line);
+                final var reversedMatcher = reversedPattern.matcher(reversedLine);
+
+
+                matcher.find();
+                reversedMatcher.find();
+
+                final var first = matcher.group();
+                final var second = reversedMatcher.group();
+
+                final var firstNumber = patterns.get(first);
+                final var secondNumber = patterns.getOrDefault(reverse(second), firstNumber);
+
+                return firstNumber.concat(secondNumber);
+
+            })
+            .mapToInt(Integer::valueOf)
+            .sum();
+        });
+
+        return String.valueOf(total);
+
+    }
+
 }
