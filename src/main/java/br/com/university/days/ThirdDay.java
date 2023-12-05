5a7fe34dcce282192f88e63a29cb5f1a9dca7eda
Geovani Granieri <geovani.granieri@outlook.com>
2023-12-05 10:30:43 -0300

    complete third day

--- src/main/java/br/com/university/days/ThirdDay.java
+++ src/main/java/br/com/university/days/ThirdDay.java
@@ -1,5 +1,13 @@
 package br.com.university.days;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+
+import br.com.university.utils.DayReview;
 import br.com.university.utils.ProblemReader;
 
 public class ThirdDay implements PuzzleResolver {
@@ -49,34 +57,152 @@ public class ThirdDay implements PuzzleResolver {
         });
     }
 
-    record Number(int number, int row, int start, int end){}
-    record Symbol(char character, int row, int start, int end){}
+
+    final class Engine {
+
+        private final String name;
+        private int start;
+        private int end;
+
+        private int count;
+        private long total;
+        private long ratio = 1L;
+
+        public Engine(int start, int end){
+            this.name = "Engine_[%d][%d]".formatted(start, end);
+            this.start = start;
+            this.end = end;
+            this.count = 0;
+        }
+
+        public void add(Long value){
+            this.total += value;
+            this.count += 1;
+            this.ratio *= value;
+        }
+
+        public void add(String value){
+            add(Long.valueOf(value));
+        }
+
+        public boolean hasOnlyTwoMembers(){
+            return this.count == 2;
+        }
+
+        public Long total(){
+            return total;
+        }
+
+        public Long ratio(){
+            return ratio;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, start, end);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Engine other){
+                return name.equals(other.name);
+            }
+
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return """
+                {
+                    "start": %s,
+                    "end": %s,
+                    "total": %d,
+                    "count": %d
+                }        
+            """.formatted(start, end, total, count);
+        }
+
+    }
+    
 
     @Override
+    @DayReview(thought = """
+        Estou 2 dias atrasados e não quero perder muito tempo nesse problema...
+        
+        Maneira mais inteligente de resolver:
+
+        1) Encapsular o "Engine" para conter um HashMap<Coordenada, Pair<Long, Total>>
+        2) Não usar ArrayList
+        3) Melhorar o método "isAdjacentToASymbol"
+        
+        Mas por hoje isso funciona...
+
+    """)
     public String solveCompleteProblem() {
         
         return ProblemReader.read("problems/day-three", lines -> {
 
             char[][] matrix = lines.map(line -> line.toCharArray()).toArray(char[][]::new);
 
-            var total = 0L;
+            var adjacency = new ArrayList<Engine>();
 
             for (var row = 0; row < matrix.length; row++) {
 
                 var number = new StringBuilder();
-                var isAdjacent = false;
+                var rowAdjancency = new HashSet<Engine>();
 
                 for (var column = 0; column < matrix[row].length; column++) {
 
                     var current = matrix[row][column];
 
-                    if (current == '.'){
+                    if (Character.isDigit(current)) {
 
+                        number.append(current);
+
+                        isAdjacentToASymbol(matrix, row, column, (character) -> character == '*', (symbolRow, symbolColumn) -> {
+                            if (symbolRow == null || symbolColumn == null) return Optional.<Engine>empty();
+
+                            final var engine = new Engine(symbolRow, symbolColumn);
+
+                            return Optional.of(engine);
+                        })
+                        .ifPresent(engine -> {
+                            rowAdjancency.add(engine);
+                        });
+
+                        if(column < matrix[row].length -1 ){
+                            continue;
+                        }
                     }
 
-                }
+                    if (!number.isEmpty()){
+                        
+                        for (var engine : rowAdjancency){
 
+                            engine.add(number.toString());
+                            
+                            adjacency
+                                .stream()
+                                .filter(it -> it.equals(engine))
+                                .findFirst()
+                                .ifPresentOrElse(
+                                    it -> it.add(engine.total()),
+                                    () -> adjacency.add(engine));
+
+                        }
+
+                        number.delete(0, number.length());
+                        rowAdjancency.clear();
+                    }
+                }
             }
+
+            final var total = adjacency
+                .stream()
+                .filter(Engine::hasOnlyTwoMembers)
+                .mapToLong(Engine::ratio)
+                .sum();
 
             return String.valueOf(total);
         });
@@ -87,6 +213,10 @@ public class ThirdDay implements PuzzleResolver {
     }
 
     private static boolean isAdjacentToASymbol(char[][] matrix, int row, int column) {
+        return isAdjacentToASymbol(matrix, row, column, ThirdDay::isASymbol, (symbolRow, symbolColumn) -> Objects.nonNull(symbolColumn) && Objects.nonNull(symbolColumn));
+    }
+
+    private static <T> T isAdjacentToASymbol(char[][] matrix, int row, int column, Predicate<Character> filter, BiFunction<Integer, Integer, T> parser) {
         final var top = Math.max(0, row - 1);
         final var bottom = Math.min(matrix.length - 1, row + 1);
         
@@ -94,47 +224,47 @@ public class ThirdDay implements PuzzleResolver {
         final var left = Math.max(0, column - 1);
 
         
-        if (isASymbol(matrix[top][column])){
-            return true;
+        if (filter.test(matrix[top][column])){
+            return parser.apply(top, column);
         }
 
-        if (isASymbol(matrix[bottom][column])){
-            return true;
+        if (filter.test(matrix[bottom][column])){
+            return parser.apply(bottom, column);
         }
 
-        if (isASymbol(matrix[row][right])){
-            return true;
+        if (filter.test(matrix[row][right])){
+            return parser.apply(row, right);
         }
 
-        if (isASymbol(matrix[row][left])){
-            return true;
-
-        }
-
-        if (isASymbol(matrix[top][right])){
-            return true;
+        if (filter.test(matrix[row][left])){
+            return parser.apply(row, left);
 
         }
 
-        if (isASymbol(matrix[top][left])){
-            return true;
+        if (filter.test(matrix[top][right])){
+            return parser.apply(top, right);
 
         }
 
-        if (isASymbol(matrix[bottom][right])){
-            return true;
+        if (filter.test(matrix[top][left])){
+            return parser.apply(top, left);
 
         }
 
-        if (isASymbol(matrix[bottom][left])){
-            return true;
+        if (filter.test(matrix[bottom][right])){
+            return parser.apply(bottom, right);
+
         }
 
-        return false;
+        if (filter.test(matrix[bottom][left])){
+            return parser.apply(bottom, left);
+        }
+
+        return parser.apply(null, null);
     }
 
     public static void main(String[] args) {
-        
+        System.out.println(new ThirdDay().solveCompleteProblem());
     }
 
 }
---
+++ src/main/java/br/com/university/utils/DayReview.java
@@ -0,0 +1,7 @@
+package br.com.university.utils;
+
+public @interface DayReview {
+    String thought = "";
+
+    String thought();
+}
